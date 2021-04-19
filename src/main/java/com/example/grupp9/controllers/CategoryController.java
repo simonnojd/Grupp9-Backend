f5fce62c1324e61646835556162744d159d650c7
yadonx <ej_yo@hotmail.com>
2021-04-19 13:48:35 +0200

    test

--- src/main/java/com/example/grupp9/controllers/CategoryController.java
+++ src/main/java/com/example/grupp9/controllers/CategoryController.java
@@ -6,10 +6,7 @@ import com.example.grupp9.repositories.CategoryRepository;
 import com.example.grupp9.repositories.CompanyRepository;
 import com.example.grupp9.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/categories")
@@ -18,8 +15,8 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @GetMapping(path = "/add/{name}")
-    public String addCategory(@PathVariable String name) {
+    @PostMapping(path = "/add")
+    public String addCategory(@RequestBody  String name) {
         Category category = categoryRepository.findByName(name);
         if (category != null)
             return "Category Exist";
@@ -38,6 +35,12 @@ public class CategoryController {
         Category category = categoryRepository.findById(id).get();
         categoryRepository.delete(category);
         return "Removed category";
+    }
+
+    @GetMapping(path = "/delAll")
+    public String removeAll(){
+        categoryRepository.deleteAll();
+        return "deleted";
     }
 
 }
