ccf858ef0c81225fdb3dbfc421ad854f7d4f5b44
Philip Zamayeri <Philip.Zamayeri@gmail.com>
2021-04-20 10:12:51 +0200

    added quantity to Product and made mockdata with @Beans

--- src/main/java/com/example/grupp9/Grupp9Application.java
+++ src/main/java/com/example/grupp9/Grupp9Application.java
@@ -1,7 +1,16 @@
 package com.example.grupp9;
 
+import com.example.grupp9.models.Category;
+import com.example.grupp9.models.Company;
+import com.example.grupp9.models.Product;
+import com.example.grupp9.models.ProductQuantity;
+import com.example.grupp9.repositories.CategoryRepository;
+import com.example.grupp9.repositories.CompanyRepository;
+import com.example.grupp9.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Grupp9Application {
@@ -10,4 +19,63 @@ public class Grupp9Application {
         SpringApplication.run(Grupp9Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner companyMockData(CompanyRepository repository){
+        return args -> {
+            Company company1 = new Company("Coca cola company");
+            Company company2 = new Company("Pringles");
+            Company company3 = new Company("Estrella");
+            Company company4 = new Company("Scan");
+            Company company5 = new Company("Uncle Ben");
+            Company company6 = new Company("Gränsbo Potatis AB");
+
+            repository.save(company1);
+            repository.save(company2);
+            repository.save(company3);
+            repository.save(company4);
+            repository.save(company5);
+            repository.save(company6);
+
+            for(Company company : repository.findAll()){
+                System.out.println(company.toString());
+            }
+        };
+    }
+
+    @Bean
+    public CommandLineRunner productMockData(ProductRepository repository){
+        return args -> {
+            Product product01 = new Product("Coca cola", 15, 100, "Läskedryck med smak av cola", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alko.fi%2Fsv%2Fprodukter%2F720393%2FCoca-Cola%2F&psig=AOvVaw2AHf_RmEA7Puj2JRK0lT2C&ust=1618990180367000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODT78amjPACFQAAAAAdAAAAABAI");
+            Product product02 = new Product("Ris", 10, 100, "Uncle bens odlade ris", "https://i2.wp.com/traningslara.se/wp-content/uploads/2013/04/potatis.nyttigt1.jpg?fit=850%2C448&ssl=1");
+            Product product03 = new Product("Spagetti", 19, 100, "Pasta från Italien", "https://outofhome.se/media/catalog/product/cache/30/image/600x/9df78eab33525d08d6e5fb8d27136e95/1/0/100048-141413_spaghetti_2.jpg");
+            Product product04 = new Product("Köttbullar", 22, 100, "Handgjorda från mamma scan hon själv", "https://www.scan.se/contentassets/4c730ee428b445c1bf1b935befc50ebb/700067_scan_mammas-kottb.350gx12-srs_vinkel2.png?preset=scan-1:1" );
+
+            repository.save(product01);
+            repository.save(product02);
+            repository.save(product03);
+            repository.save(product04);
+
+            for(Product product : repository.findAll()){
+                System.out.println(product.toString());
+            }
+        };
+    }
+
+    @Bean
+    public CommandLineRunner categoryMockData(CategoryRepository repository){
+        return args -> {
+            Category category01 = new Category("Läskedryck");
+            Category category02 = new Category("Skafferi");
+            Category category03 = new Category("Husmanskost");
+
+            repository.save(category01);
+            repository.save(category02);
+            repository.save(category03);
+
+            for(Category category : repository.findAll()){
+                System.out.println(category.toString());
+            }
+
+        };
+    }
 }
