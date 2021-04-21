cfec76e1718ab639601f3fc5ca09650355f161b6
Liliana Montini Pitra <liliana.pitra@gmail.com>
2021-04-21 10:07:05 +0200

    cross origin for CategoryController

--- src/main/java/com/example/grupp9/controllers/CategoryController.java
+++ src/main/java/com/example/grupp9/controllers/CategoryController.java
@@ -6,12 +6,10 @@ import com.example.grupp9.repositories.CategoryRepository;
 import com.example.grupp9.repositories.CompanyRepository;
 import com.example.grupp9.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/categories")
 public class CategoryController {
 
